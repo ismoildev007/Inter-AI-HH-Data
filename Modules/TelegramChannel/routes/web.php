56727544076c31fba0867d7449d9a325daa172e7
laraveldev <elnurbek.pubgmobile@gmail.com>
2025-09-15 10:07:06 +0500

    ui

--- Modules/TelegramChannel/routes/web.php
+++ Modules/TelegramChannel/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\TelegramChannel\Http\Controllers\ChannelsPageController;
 
-// No web routes for API-only module
-
+Route::prefix('telegram/channels')->name('telegram.channels.')->group(function () {
+    Route::get('manage', [ChannelsPageController::class, 'index'])->name('index');
+    Route::get('create', [ChannelsPageController::class, 'create'])->name('create');
+    Route::post('', [ChannelsPageController::class, 'store'])->name('store');
+});
