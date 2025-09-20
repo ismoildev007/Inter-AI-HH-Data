a059155068075d94dad3b71cead0456b84a6963a
Ismoil Dev <ismoil3436@gmail.com>
2025-09-20 18:04:51 +0500

    user edit

--- Modules/Users/Http/Controllers/AuthController.php
+++ Modules/Users/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace Modules\Users\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use App\Traits\ApiResponseTrait;
 use Illuminate\Http\Request;
 use Modules\Users\Http\Requests\LoginRequest;
@@ -29,17 +30,33 @@ class AuthController extends Controller
 
         return response()->json($result, 201);
     }
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        $result = $this->repo->update($user, $request->all());
+
+        if (isset($result['status']) && $result['status'] === 'error') {
+            return $this->error($result['message'], $result['code']);
+        }
+
+        return response()->json($result, 200);
+    }
 
     public function login(LoginRequest $request)
     {
         $result = $this->repo->login($request->validated());
 
         if (!$result) {
-            return $this->error('Invalid credentials', 401);
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
         }
 
-
-        return response()->json($result);
+        return response()->json([
+            'status' => 'success',
+            'data'   => $result
+        ], 200);
     }
 
     public function me(Request $request)
