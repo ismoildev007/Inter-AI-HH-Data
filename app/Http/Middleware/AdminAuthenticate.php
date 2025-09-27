7847f09b841ace67414bb63d8dee0c5658cd95cf
laraveldev <elnurbek.pubgmobile@gmail.com>
2025-09-27 13:00:50 +0500

    b

---
+++ app/Http/Middleware/AdminAuthenticate.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+
+class AdminAuthenticate extends Middleware
+{
+    protected function redirectTo($request)
+    {
+        if (! $request->expectsJson()) {
+            return route('admin.login');
+        }
+        return null;
+    }
+}
+
