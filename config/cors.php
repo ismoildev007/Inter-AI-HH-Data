d241622a2d2baf847770ad3e4b2885251352e358
Quvonchbek <jalilovquvonchbek4@gmail.com>
2025-10-06 13:38:31 +0500

    test

--- config/cors.php
+++ config/cors.php
@@ -1,15 +1,15 @@
 <?php
 
-// return [
-//     'paths' => ['api/*'],
-//     'allowed_methods' => ['*'],
-//     'allowed_origins' => array_filter([
-//         env('FRONTEND_URL', 'http://localhost:5173'),
-//     ]),
-//     'allowed_origins_patterns' => [],
-//     'allowed_headers' => ['*'],
-//     'exposed_headers' => [],
-//     'max_age' => 3600,
-//     'supports_credentials' => true,
-// ];
+return [
+    'paths' => ['api/*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => array_filter([
+        env('FRONTEND_URL', 'http://localhost:5173'),
+    ]),
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 3600,
+    'supports_credentials' => true,
+];
 
