a011be49cf182b3b7c2d5bdf7ace1a9952f47bde
laraveldev <elnurbek.pubgmobile@gmail.com>
2025-10-11 14:48:53 +0500

    admn

--- Modules/Admin/resources/views/Admin/Dashboard/dashboard.blade.php
+++ Modules/Admin/resources/views/Admin/Dashboard/dashboard.blade.php
@@ -11,16 +11,6 @@
             <li class="breadcrumb-item">Dashboard</li>
         </ul>
     </div>
-    <!-- <div class="page-header-right ms-auto">
-        <div class="d-flex align-items-center gap-2">
-            <div id="reportrange" class="reportrange-picker d-flex align-items-center">
-                <span class="reportrange-picker-field"></span>
-            </div>
-            <a href="javascript:void(0);" class="btn btn-md btn-light-brand">
-                <i class="feather-filter me-2"></i> Manage Filter
-            </a>
-        </div>
-    </div> -->
 </div>
 
 <div class="main-content">
@@ -30,66 +20,86 @@
             <div class="card h-100">
                 <div class="card-body d-flex justify-content-between align-items-center">
                     <div class="d-flex gap-3 align-items-center">
-                        <div class="avatar-text avatar-lg bg-gray-200"><i class="feather-users"></i></div>
+                        <a href="{{ route('admin.users.index') }}" class="nxl-link">
+                            <div class="avatar-text avatar-lg bg-gray-200">
+                                <i class="feather-users"></i>
+                            </div>
+                        </a>
                         <div>
                             <h4 class="fw-bold mb-0">{{ $usersCount }}</h4>
                             <small class="text-muted">Users</small>
                         </div>
                     </div>
-                    
                 </div>
             </div>
         </div>
+
         <div class="col-xxl-3 col-md-6">
             <div class="card h-100">
                 <div class="card-body d-flex justify-content-between align-items-center">
                     <div class="d-flex gap-3 align-items-center">
-                        <div class="avatar-text avatar-lg bg-gray-200"><i class="feather-file-text"></i></div>
+                        <a href="{{ route('admin.resumes.index') }}" class="nxl-link">
+                            <div class="avatar-text avatar-lg bg-gray-200">
+                                <i class="feather-file-text"></i>
+                            </div>
+                        </a>
                         <div>
                             <h4 class="fw-bold mb-0">{{ $resumesCount }}</h4>
                             <small class="text-muted">Resumes</small>
                         </div>
                     </div>
-                    
                 </div>
             </div>
         </div>
+
         <div class="col-xxl-3 col-md-6">
             <div class="card h-100">
                 <div class="card-body d-flex justify-content-between align-items-center">
                     <div class="d-flex gap-3 align-items-center">
-                        <div class="avatar-text avatar-lg bg-gray-200"><i class="feather-briefcase"></i></div>
+                        <a href="{{ route('admin.applications.index') }}" class="nxl-link">
+                            <div class="avatar-text avatar-lg bg-gray-200">
+                                <i class="feather-briefcase"></i>
+                            </div>
+                        </a>
                         <div>
                             <h4 class="fw-bold mb-0">{{ $applicationsCount }}</h4>
                             <small class="text-muted">Applications</small>
                         </div>
                     </div>
-                    
                 </div>
             </div>
         </div>
+
         <div class="col-xxl-3 col-md-6">
             <div class="card h-100">
                 <div class="card-body d-flex justify-content-between align-items-center">
                     <div class="d-flex gap-3 align-items-center">
-                        <div class="avatar-text avatar-lg bg-gray-200"><i class="feather-send"></i></div>
+                        <a href="{{ route('admin.telegram_channels.index') }}" class="nxl-link">
+                            <div class="avatar-text avatar-lg bg-gray-200">
+                                <i class="feather-send"></i>
+                            </div>
+                        </a>
                         <div>
                             <h4 class="fw-bold mb-0">{{ $telegramChannelsCount }}</h4>
                             <small class="text-muted">Telegram Channels</small>
                         </div>
                     </div>
-                    
                 </div>
             </div>
         </div>
     </div>
 
+    <!-- 👇 pastdagi barcha qismlar sizdagi original ko‘rinishda qoldi -->
     <!-- Visitors + Tasks -->
     <div class="row g-3 mt-1">
         <div class="col-xxl-8">
             <div class="card h-100">
-                <div class="card-header"><h5 class="mb-0">Visitors Overview</h5></div>
-                <div class="card-body"><div id="visitors-overview-chart"></div></div>
+                <div class="card-header">
+                    <h5 class="mb-0">Visitors Overview</h5>
+                </div>
+                <div class="card-body">
+                    <div id="visitors-overview-chart"></div>
+                </div>
             </div>
         </div>
         <div class="col-xxl-4">
@@ -131,86 +141,75 @@
         </div>
     </div>
 
-    <!-- Campaign -->
-    <!-- <div class="row g-3 mt-1">
-        <div class="col-xxl-4">
-            <div class="card h-100">
-                <div class="card-header"><h5 class="mb-0">Campaign Analytics</h5></div>
-                <div class="card-body"><div id="campaign-alytics-bar-chart"></div></div>
-            </div>
-        </div>
-    </div> -->
-
     <!-- Metrics + Leads + Team -->
     <div class="row g-3 mt-1">
         <div class="col-xxl-3 col-md-6">
-            <div class="card h-100"><div class="card-body"><h6>Hourly Visitors</h6><div id="bounce-rate"></div></div></div>
-        </div>
-        <div class="col-xxl-3 col-md-6">
-            <div class="card h-100"><div class="card-body"><h6>Daily Visitors</h6><div id="page-views"></div></div></div>
-        </div>
-        <div class="col-xxl-3 col-md-6">
-            <div class="card h-100"><div class="card-body"><h6>Monthly Visitors</h6><div id="site-impressions"></div></div></div>
-        </div>
-        <div class="col-xxl-3 col-md-6">
-            <div class="card h-100"><div class="card-body"><h6>Yearly Visitors</h6><div id="conversions-rate"></div></div></div>
-        </div>
-        <!-- <div class="col-xxl-8 col-12">
             <div class="card h-100">
-                <div class="card-header"><h5 class="mb-0">Leads Overview</h5></div>
-                <div class="card-body"><div id="leads-overview-donut"></div></div>
-            </div>
-        </div> -->
-        <!-- <div class="col-xxl-4 col-12">
-            <div class="card h-100">
-                <div class="card-header"><h5 class="mb-0">Team Progress</h5></div>
                 <div class="card-body">
-                    <div class="hstack justify-content-between border rounded-3 p-2 mb-2">
-                        <div class="d-flex gap-2 align-items-center">
-                            <img src="{{ module_vite('build-admin','resources/assets/js/app.js')->asset('resources/assets/images/avatar/5.svg') }}" class="img-fluid rounded-circle" width="35">
-                            <div><b>James Cameron</b><br><small class="text-muted">Frontend</small></div>
-                        </div>
-                        <div class="team-progress-1"></div>
-                    </div>
-                    <div class="hstack justify-content-between border rounded-3 p-2 mb-2">
-                        <div class="d-flex gap-2 align-items-center">
-                            <img src="{{ module_vite('build-admin','resources/assets/js/app.js')->asset('resources/assets/images/avatar/2.png') }}" class="img-fluid rounded-circle" width="35">
-                            <div><b>Archie Cantones</b><br><small class="text-muted">UI/UX</small></div>
-                        </div>
-                        <div class="team-progress-2"></div>
-                    </div>
-                    <div class="hstack justify-content-between border rounded-3 p-2 mb-2">
-                        <div class="d-flex gap-2 align-items-center">
-                            <img src="{{ module_vite('build-admin','resources/assets/js/app.js')->asset('resources/assets/images/avatar/3.png') }}" class="img-fluid rounded-circle" width="35">
-                            <div><b>Malanie Hanvey</b><br><small class="text-muted">Backend</small></div>
-                        </div>
-                        <div class="team-progress-3"></div>
-                    </div>
-                    <div class="hstack justify-content-between border rounded-3 p-2">
-                        <div class="d-flex gap-2 align-items-center">
-                            <img src="{{ module_vite('build-admin','resources/assets/js/app.js')->asset('resources/assets/images/avatar/4.png') }}" class="img-fluid rounded-circle" width="35">
-                            <div><b>Kenneth Hune</b><br><small class="text-muted">Marketer</small></div>
-                        </div>
-                        <div class="team-progress-4"></div>
-                    </div>
+                    <h6>Hourly Visitors</h6>
+                    <div id="bounce-rate"></div>
                 </div>
             </div>
-        </div> -->
+        </div>
+        <div class="col-xxl-3 col-md-6">
+            <div class="card h-100">
+                <div class="card-body">
+                    <h6>Daily Visitors</h6>
+                    <div id="page-views"></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xxl-3 col-md-6">
+            <div class="card h-100">
+                <div class="card-body">
+                    <h6>Monthly Visitors</h6>
+                    <div id="site-impressions"></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xxl-3 col-md-6">
+            <div class="card h-100">
+                <div class="card-body">
+                    <h6>Yearly Visitors</h6>
+                    <div id="conversions-rate"></div>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <!-- Vacancies analytics (Hourly, Daily, Weekly, Monthly) -->
+    <!-- Vacancies analytics -->
     <div class="row g-3 mt-1">
         <div class="col-xxl-3 col-md-6">
-            <div class="card h-100"><div class="card-body"><h6>Hourly Vacancies</h6><div id="vacancy-hourly"></div></div></div>
+            <div class="card h-100">
+                <div class="card-body">
+                    <h6>Hourly Vacancies</h6>
+                    <div id="vacancy-hourly"></div>
+                </div>
+            </div>
         </div>
         <div class="col-xxl-3 col-md-6">
-            <div class="card h-100"><div class="card-body"><h6>Daily Vacancies</h6><div id="vacancy-daily"></div></div></div>
+            <div class="card h-100">
+                <div class="card-body">
+                    <h6>Daily Vacancies</h6>
+                    <div id="vacancy-daily"></div>
+                </div>
+            </div>
         </div>
         <div class="col-xxl-3 col-md-6">
-            <div class="card h-100"><div class="card-body"><h6>Weekly Vacancies</h6><div id="vacancy-weekly"></div></div></div>
+            <div class="card h-100">
+                <div class="card-body">
+                    <h6>Weekly Vacancies</h6>
+                    <div id="vacancy-weekly"></div>
+                </div>
+            </div>
         </div>
         <div class="col-xxl-3 col-md-6">
-            <div class="card h-100"><div class="card-body"><h6>Monthly Vacancies</h6><div id="vacancy-monthly"></div></div></div>
+            <div class="card h-100">
+                <div class="card-body">
+                    <h6>Monthly Vacancies</h6>
+                    <div id="vacancy-monthly"></div>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -218,21 +217,23 @@
     <div class="row g-3 mt-1">
         <div class="col-xxl-4">
             <div class="card h-100">
-                <div class="card-header"><h5 class="mb-0">Top Visitors</h5></div>
+                <div class="card-header">
+                    <h5 class="mb-0">Top Visitors</h5>
+                </div>
                 <div class="card-body">
                     @forelse($topUsers as $u)
-                        <div class="d-flex justify-content-between border-bottom py-2 align-items-center">
-                            <div class="d-flex gap-2 align-items-center">
-                                <div class="avatar-text bg-primary text-white"><i class="feather-user"></i></div>
-                                <div>
-                                    <b>{{ trim(($u->first_name ?? '').' '.($u->last_name ?? '')) ?: ($u->email ?? 'User #'.$u->id) }}</b><br>
-                                    <small class="text-muted">{{ $u->email }}</small>
-                                </div>
+                    <div class="d-flex justify-content-between border-bottom py-2 align-items-center">
+                        <div class="d-flex gap-2 align-items-center">
+                            <div class="avatar-text bg-primary text-white"><i class="feather-user"></i></div>
+                            <div>
+                                <b>{{ trim(($u->first_name ?? '').' '.($u->last_name ?? '')) ?: ($u->email ?? 'User #'.$u->id) }}</b><br>
+                                <small class="text-muted">{{ $u->email }}</small>
                             </div>
-                            <div class="text-end"><b>{{ $u->visits_count }}</b></div>
                         </div>
+                        <div class="text-end"><b>{{ $u->visits_count }}</b></div>
+                    </div>
                     @empty
-                        <div class="text-muted">No visitor data yet</div>
+                    <div class="text-muted">No visitor data yet</div>
                     @endforelse
                 </div>
                 <a href="{{ route('admin.visits.top_users') }}" class="card-footer text-center">View All</a>
@@ -240,17 +241,19 @@
         </div>
         <div class="col-xxl-4">
             <div class="card h-100">
-                <div class="card-header"><h5 class="mb-0">Vacancies by Category @isset($vacanciesTotal)<span class="text-muted fs-12">(Total: {{ $vacanciesTotal }})</span>@endisset</h5></div>
+                <div class="card-header">
+                    <h5 class="mb-0">Vacancies by Category @isset($vacanciesTotal)<span class="text-muted fs-12">(Total: {{ $vacanciesTotal }})</span>@endisset</h5>
+                </div>
                 <div class="card-body">
                     @if(!empty($vacancyCategories) && count($vacancyCategories))
-                        @foreach($vacancyCategories as $row)
-                            <div class="d-flex justify-content-between border-bottom py-2 align-items-center">
-                                <div class="text-capitalize">{{ $row->category ?: 'other' }}</div>
-                                <div class="fw-bold">{{ $row->c }}</div>
-                            </div>
-                        @endforeach
+                    @foreach($vacancyCategories as $row)
+                    <div class="d-flex justify-content-between border-bottom py-2 align-items-center">
+                        <div class="text-capitalize">{{ $row->category ?: 'other' }}</div>
+                        <div class="fw-bold">{{ $row->c }}</div>
+                    </div>
+                    @endforeach
                     @else
-                        <div class="text-muted">No vacancy data yet</div>
+                    <div class="text-muted">No vacancy data yet</div>
                     @endif
                 </div>
                 <a href="{{ route('admin.vacancies.categories') }}" class="card-footer text-center">View All</a>
@@ -258,17 +261,22 @@
         </div>
         <div class="col-xxl-4">
             <div class="card h-100">
-                <div class="card-header"><h5 class="mb-0">Social Radar</h5></div>
-                <div class="card-body"><div id="social-radar-chart"></div></div>
+                <div class="card-header">
+                    <h5 class="mb-0">Social Radar</h5>
+                </div>
+                <div class="card-body">
+                    <div id="social-radar-chart"></div>
+                </div>
             </div>
         </div>
     </div>
-  <script>
+</div>
+
+<script>
     window.visitorsChart = {
         labels: @json($visitorsLabels ?? []),
         series: @json($visitorsSeries ?? [])
     };
-    window.totalVisits = (window.visitorsChart.series || []).reduce((a,b)=>a+(+b||0),0);
     window.dashboardMini = {
         labels: @json($dayLabels ?? []),
         series: {
@@ -278,6 +286,5 @@
         }
     };
     window.analyticsData = @json($analyticsData ?? []);
-  </script>
-</div><!-- /.main-content -->
+</script>
 @endsection
