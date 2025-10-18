7b463e53240409813f13c003935b9a8cd26dc63d
Quvonchbek <jalilovquvonchbek4@gmail.com>
2025-10-18 17:00:40 +0500

    test

--- Modules/Vacancies/Services/VacancyMatchingService.php
+++ Modules/Vacancies/Services/VacancyMatchingService.php
@@ -166,6 +166,7 @@ class VacancyMatchingService
                 'weight_embed'   => 0.75,
                 'weight_jaccard' => 0.15,
                 'weight_cov'     => 0.1,
+                "title_threshold" => 0.5
             ]);
 
         Log::info('Fetch HH details took: ' . (microtime(true) - $start) . 's');
