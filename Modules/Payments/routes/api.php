fc0bffa0d99d39ecbc3f3dc1f06fad090928d577
Ismoil Dev <ismoil3436@gmail.com>
2025-10-22 14:16:59 +0500

    Payme Integration

--- Modules/Payments/routes/api.php
+++ Modules/Payments/routes/api.php
@@ -3,14 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Payments\Http\Controllers\PaymeController;
 use Modules\Payments\Http\Controllers\ClickController;
-use Modules\Payments\Http\Controllers\PaymentsController;
 
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::post('payme/callback', [PaymeController::class, 'handleCallback']);
-    Route::get('payme/booking', [PaymeController::class, 'booking']);
-// Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-//     Route::apiResource('payments', PaymentsController::class)->names('payments');
-// });
+Route::middleware(['auth:sanctum'])->prefix('payme')->group(function () {
+    Route::post('callback', [PaymeController::class, 'handleCallback']);
+    Route::get('booking', [PaymeController::class, 'booking']);
+ });
 
 Route::prefix('payment/click')->group(function () {
     Route::post('/prepare', [ClickController::class, 'prepare']);
