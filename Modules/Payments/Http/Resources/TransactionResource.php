04222d308dbc95a26af307565611e3eb793503d2
Ismoil Dev <ismoil3436@gmail.com>
2025-10-22 19:32:28 +0500

    Payme Integration

--- Modules/Payments/Http/Resources/TransactionResource.php
+++ Modules/Payments/Http/Resources/TransactionResource.php
@@ -3,8 +3,9 @@
 namespace Modules\Payments\Http\Resources;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class TransactionResource
+class TransactionResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -21,8 +22,12 @@ class TransactionResource
                 'transaction_id' => (int) ($this->id),
             ],
             'create_time' => isset($this->create_time) ? (int) $this->create_time : 0,
-            'perform_time' => is_numeric($this->perform_time) ? (int) $this->perform_time : (strtotime($this->perform_time) * 1000),
-            'cancel_time' => is_numeric($this->cancel_time) ? (int) $this->cancel_time : (strtotime($this->cancel_time) * 1000),
+            'perform_time' => is_numeric($this->perform_time)
+                ? (int) $this->perform_time
+                : (strtotime($this->perform_time) * 1000),
+            'cancel_time' => is_numeric($this->cancel_time)
+                ? (int) $this->cancel_time
+                : (strtotime($this->cancel_time) * 1000),
             'transaction' => (int) ($this->id),
             'state' => isset($this->state) ? (int) $this->state : 0,
             'reason' => !empty($this->reason) ? (int) $this->reason : null,
