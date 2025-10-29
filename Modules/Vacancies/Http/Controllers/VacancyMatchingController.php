fd2176388f7e13092f23d291015a745aa67627d5
Ismoil Dev <ismoil3436@gmail.com>
2025-10-29 14:15:14 +0500

    Resumeni categoriyalab filtirlandi

--- Modules/Vacancies/Http/Controllers/VacancyMatchingController.php
+++ Modules/Vacancies/Http/Controllers/VacancyMatchingController.php
@@ -64,16 +64,30 @@ class VacancyMatchingController extends Controller
 
         $resumeIds = $user->resumes()->pluck('id');
 
-        $results = MatchResult::with('vacancy.employer')
+//        $results = MatchResult::with('vacancy.employer')
+//            ->leftJoin('applications', function ($join) use ($user) {
+//                $join->on('applications.vacancy_id', '=', 'match_results.vacancy_id')
+//                    ->where('applications.user_id', $user->id);
+//            })
+//            ->whereIn('match_results.resume_id', $resumeIds)
+//            ->orderByRaw('CASE WHEN applications.id IS NULL THEN 0 ELSE 1 END ASC')
+//            ->orderByDesc('match_results.score_percent')
+//            ->select('match_results.*')
+//            ->get();
+        $results = MatchResult::query()
+            ->leftJoin('vacancies', 'vacancies.id', '=', 'match_results.vacancy_id')
             ->leftJoin('applications', function ($join) use ($user) {
                 $join->on('applications.vacancy_id', '=', 'match_results.vacancy_id')
                     ->where('applications.user_id', $user->id);
             })
             ->whereIn('match_results.resume_id', $resumeIds)
+            ->orderByRaw('CASE WHEN vacancies.source = "hh" THEN 1 WHEN vacancies.source = "telegram" THEN 0 ELSE -1 END DESC')
             ->orderByRaw('CASE WHEN applications.id IS NULL THEN 0 ELSE 1 END ASC')
             ->orderByDesc('match_results.score_percent')
             ->select('match_results.*')
+            ->with('vacancy.employer')
             ->get();
+
 
         return response()->json([
             'status'  => 'success',
