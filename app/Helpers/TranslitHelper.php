89dc3a04d83b7d196c11776817e4baa3f0cac9b7
Ismoil Dev <ismoil3436@gmail.com>
2025-10-29 10:18:11 +0500

    Prompt yechimini qvomman

--- app/Helpers/TranslitHelper.php
+++ app/Helpers/TranslitHelper.php
@@ -12,8 +12,21 @@ class TranslitHelper
         'а','б','д','е','ф','г','ҳ','и','ж','к','л','м','н','о','п','қ','р','с','т','у','в','х','й','з',"ъ","‘","’","ʼ","`","‘","’"
     ];
 
-    public static function toCyrillic(string $text): string
+    public static function toLatin(?string $text): string
     {
+        $text = $text ?? ''; // null bo‘lsa bo‘sh string
+        $map = [
+            'я' => 'ya','ю' => 'yu','ч' => 'ch','ш' => 'sh','ё' => 'yo','ў' => 'o‘','ғ' => 'g‘',
+            'Я' => 'Ya','Ю' => 'Yu','Ч' => 'Ch','Ш' => 'Sh','Ё' => 'Yo','Ў' => 'O‘','Ғ' => 'G‘',
+        ];
+
+        $text = strtr($text, $map);
+        return str_replace(self::$cyrillic, self::$latin, $text);
+    }
+
+    public static function toCyrillic(?string $text): string
+    {
+        $text = $text ?? '';
         $map = [
             'ya' => 'я','yu' => 'ю','ch' => 'ч','sh' => 'ш','yo' => 'ё','o‘' => 'ў','g‘' => 'ғ',
             'Ya' => 'Я','Yu' => 'Ю','Ch' => 'Ч','Sh' => 'Ш','Yo' => 'Ё','O‘' => 'Ў','G‘' => 'Ғ',
@@ -23,14 +36,4 @@ class TranslitHelper
         return str_replace(self::$latin, self::$cyrillic, $text);
     }
 
-    public static function toLatin(string $text): string
-    {
-        $map = [
-            'я' => 'ya','ю' => 'yu','ч' => 'ch','ш' => 'sh','ё' => 'yo','ў' => 'o‘','ғ' => 'g‘',
-            'Я' => 'Ya','Ю' => 'Yu','Ч' => 'Ch','Ш' => 'Sh','Ё' => 'Yo','Ў' => 'O‘','Ғ' => 'G‘',
-        ];
-
-        $text = strtr($text, $map);
-        return str_replace(self::$cyrillic, self::$latin, $text);
-    }
 }
